24c4706f59454fe190e5a1b6c5bf22684d8387e6
KadricAl <almir.kadric90@gmail.com>
2024-09-06 12:30:25 +0200

    Add clear func

--- src/components/rightSide.jsx
+++ src/components/rightSide.jsx
@@ -5,14 +5,26 @@ const RightSide = ({ updateContacts }) => {
 
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
+    const [phoneError, setPhoneError] = useState(false);
 
     const handleName = (e) => {
         setName(e.target.value);
     }
 
     const handlePhone = (e) => {
-        setPhone(e.target.value);
-    }
+        
+        const value = e.target.value;
+        const isValid =  /^[+\d]{0,14}$/.test(value);
+
+        if (!isValid) {
+            setPhoneError(true);
+            setPhone(value);
+          } else {
+            setPhoneError(false);
+            setPhone(value);
+          }
+        };
+    
 
     const handleAddButton = () => {
         const savedContacts = JSON.parse(localStorage.getItem('contacts')) || [];
@@ -26,13 +38,22 @@ const RightSide = ({ updateContacts }) => {
         setName('');
         setPhone('');
     };
+
+    const handleClearButton = ()=> {
+
+        setName('');
+        setPhone('');
+
+    }
+
+
     return(
     <div className="flex flex-col gap-4 mx-4 w-full text-center">
         <span className="text-lg text-sky-600 font-medium">ADD CONTACT</span>
         <TextField value={name} onChange={handleName}  label="Contact name" variant="outlined" />
-        <TextField value={phone} onChange={handlePhone}  label="Phone number" variant="outlined" />
+        <TextField value={phone} onChange={handlePhone} error={phoneError} helperText={phoneError ? 'Please enter numbers': ''}  label="Phone number" variant="outlined" />
         <div className="flex flex-row justify-between gap-4">
-            <Button className="w-1/2" variant="outlined">CLEAR</Button>
+            <Button onClick={handleClearButton} className="w-1/2" variant="outlined">CLEAR</Button>
             <Button onClick={handleAddButton} className="w-1/2" variant="contained">ADD</Button>
         </div>
     </div>
